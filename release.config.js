6831e6d73a4324bfc2aba01128e8d70bf8a75d5e
alimajed <alimajed1991@gmail.com>
2022-01-15 22:34:11 +0200

    fix: semantic release example

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "branches": "master",
+    "repositoryURL": "https://github.com/alimajed/react-app",
+    "plugins": [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/release-npm",
+        "@semantic-release/github",
+    ]
+}
