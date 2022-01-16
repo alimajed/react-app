411c89249d7e81153b8293a3b628d627696e550f
alimajed <alimajed1991@gmail.com>
2022-01-16 10:59:56 +0200

    workflow: semantic release

--- release.config.js
+++ release.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "plugins": [
         "@semantic-release/commit-analyzer",
         "@semantic-release/release-notes-generator",
-        "@semantic-release/release-npm",
-        "@semantic-release/github",
+        "@semantic-release/npm",
+        "@semantic-release/github"
     ]
 }
